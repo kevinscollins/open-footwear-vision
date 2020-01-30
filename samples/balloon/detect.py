3ec1d4d20a118d286b65613f9fff387e088b91aa
danx0r <dan.miller@eye0.com>
2020-01-30 15:50:11 -0800

    rm unused args for klarety

--- samples/balloon/detect.py
+++ samples/balloon/detect.py
@@ -2,7 +2,7 @@ import os
 import openfootballoon
 
 cmd = """
-python3 balloon.py splash --dataset ../../mask_rcnn/datasets/balloon/ --weights %smask_rcnn_balloon.h5 --image party.jpg 
+python3 balloon.py splash --weights %smask_rcnn_balloon.h5 --image party.jpg 
 diff -s party_splashed.png splash_expected.png
 """ % openfootballoon.datapath
 
