fbda9261ec9093b41028493a1065dbee15cc1a1a
graves <genevieve.graves@eye0.com>
2020-01-31 00:47:39 -0800

    gpu option

--- init.py
+++ init.py
@@ -4,6 +4,9 @@ print ("installing requirements")
 os.system("pip install -r requirements1.txt")
 os.system("pip install -r requirements2.txt")
 
+if input("install tensorflow-gpu?").lower()[:1] == 'y':
+    os.system("pip install -r tensorflow_gpu.txt")
+
 if input("install available dataset: openfootballoon (245 Mbyte)?").lower()[:1] == 'y':
     os.system("pip install -r datasets.txt")
 
