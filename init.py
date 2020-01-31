24db18e6ddaf1d2eddf74380e5e734f91c9bad72
danx0r <dan.miller@eye0.com>
2020-01-30 16:05:22 -0800

    init.py

---
+++ init.py
@@ -0,0 +1,12 @@
+import os
+
+print ("installing requirements")
+os.system("pip install -r requirements1.txt")
+os.system("pip install -r requirements2.txt")
+
+if input("install available dataset: openfootballoon (245 Mbyte)?").lower()[:1] == 'y':
+    os.system("pip install -r datasets.txt")
+
+if input("run test?").lower()[:1] == 'y':
+    os.chdir("samples/balloon")
+    os.system("python detect.py")
