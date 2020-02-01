3c31c94a3d01bfb90be78d4eee7a50c6b9143634
danx0r <dan.miller@eye0.com>
2020-01-31 19:49:26 -0800

    ..

--- samples/balloon/train.py
+++ samples/balloon/train.py
@@ -2,7 +2,7 @@ import os
 import open_footwear_images
 
 cmd = """
-python balloon.py train --weights coco --dataset %s/test_balloons/balloon/
+python balloon.py train --weights coco --dataset %stest_balloons/balloon/
 """ % open_footwear_images.datapath
 
 print (cmd)
